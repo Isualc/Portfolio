893ef3df24782c11b721a4f6120b1dbda8602d60
Isualc <claus_hierzer@gmx.at>
2024-08-21 18:28:02 +0200

    dffd

--- javaScript/script.js
+++ javaScript/script.js
@@ -21,4 +21,4 @@ function openProjectDetails() {
          window.location.href = 'https://github.com/Isualc/Java-Projects/tree/main/SnakeGame';
    }
 }
-//sAIzaSyC1SntMm6c43zGTncpMno7novTwwb-Sx_k
+//***REDACTED-HASH***
